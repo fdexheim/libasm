40b6f0eea399f811768525fcc7dccd6150ec097c
Florian DEXHEIMER <fdexheim@e1r7p22.42.fr>
2018-11-06 12:34:01 +0100

    fixed strcat + strlen ! NOICE

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -6,7 +6,7 @@
 #    By: fdexheim <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/30 09:55:01 by fdexheim          #+#    #+#              #
-#    Updated: 2018/11/05 11:16:48 by fdexheim         ###   ########.fr        #
+#    Updated: 2018/11/06 10:07:24 by fdexheim         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,14 +19,15 @@ _ft_strlen:
 	push rdi
 
 	mov rbx, rdi
+	xor rax, rax
 	xor al, al
 	mov rcx, 0xffffffff
 	repne scasb
 	sub rdi, rbx
+	sub rdi, 1
 	mov rax, rdi
 
 	pop rdi
 	pop rcx
 	pop rbx
-	mov rax, 10 ; DELET THIS
 	ret
