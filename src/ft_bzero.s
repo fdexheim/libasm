c3f54703cd528c1887990e2119140d67eac08367
Florian DEXHEIMER <fdexheim@e1r7p22.42.fr>
2018-11-12 16:31:40 +0100

    updating test main, fixed crash bzero, fuck you strdup

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -6,7 +6,7 @@
 #    By: fdexheim <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/17 12:31:50 by fdexheim          #+#    #+#              #
-#    Updated: 2018/11/01 10:09:44 by fdexheim         ###   ########.fr        #
+#    Updated: 2018/11/12 10:34:15 by fdexheim         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,9 +24,9 @@ _ft_bzero:
 	mov rdx, 0 ;			set counter to 0
 
 loop:
+	cmp rsi, rdx ;			compare counter with size
+	je end ;				brother do you have some loops
 	mov byte[rdi], 0 ;		set address value to 0
 	inc rdx ;				inc address
 	inc rdi
-	cmp rsi, rdx ;			compare counter with size
-	jne loop ;				brother do you have some loops
-	jmp end
+	jmp loop
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: fdexheim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 14:59:48 by fdexheim          #+#    #+#             */
-/*   Updated: 2018/11/09 14:54:28 by fdexheim         ###   ########.fr       */
+/*   Updated: 2018/11/12 15:25:11 by fdexheim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,296 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int			main()
+//------------------------------------------------------------------------------
+static size_t		strlen_ghetto(const char *str)
 {
-	int				lowcase = 'a';
-	int				lowcase2 = 'z';
+	size_t			ret;
 
-	int				upcase = 'a';
-	int				upcase2 = 'Z';
+	ret = 0;
+	if (str == NULL)
+		return (0);
+	while (*str)
+	{
+		ret++;
+		str++;
+	}
+	return (ret);
+}
 
-	int				number = '0';
-	int				number2 = '9';
+//------------------------------------------------------------------------------
+void				putstr_ghetto(const char *str)
+{
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return ;
+	}
+	write(1, str, strlen_ghetto(str));
+}
 
+//------------------------------------------------------------------------------
+void				putnbr_ghetto(int n)
+{
+	char			c;
+
+	c = 0;
+	if (n == -2147483648)
+	{
+		putstr_ghetto("-2147483648");
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		write(1, "-", 1);
+	}
+	if (n >= 10)
+	{
+		putnbr_ghetto(n / 10);
+	}
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+//------------------------------------------------------------------------------
+void				put_unsigned_char_ghetto_hex(const unsigned char c)
+{
+	const char		hexa[] = "0123456789abcdef";
+	unsigned char	ol;
+
+	if (c <= 0xf)
+	{
+		write(1, "0", 1);
+		ol = hexa[c % 16];
+		write(1, &ol, 1);
+	}
+	else
+	{
+		ol = hexa[c / 16];
+		write(1, &ol, 1);
+		ol = hexa[c % 16];
+		write(1, &ol, 1);
+	}
+}
+
+//------------------------------------------------------------------------------
+void				put_size_t_ghetto(const size_t src)
+{
+	char			c;
+
+	if (src >= 10)
+		put_size_t_ghetto(src / 10);
+	c = src % 10 + '0';
+	write(1, &c, 1);
+}
+
+//------------------------------------------------------------------------------
+void				dumpmem(void *ptr, size_t len)
+{
+	size_t			i;
+	unsigned char	*topkek;
+
+	i = 0;
+	while (i < len)
+	{
+		topkek = ptr;
+		put_unsigned_char_ghetto_hex(*topkek);
+		write(1, " ", 1);
+		ptr++;
+		i++;
+	}
+}
+
+//==============================================================================
+int					main(void)
+{
 	const char		*empty_string = "";
 	const char		*short_string = "42";
 	const char		*medium_string = "You there ! Let me bend your ear for a moment !";
-	const char		*long_string = "Do you know where the deulist tradition came from, exile?\n\nThe Ezomytes.\n\nEarly on, those western potato munchers worked out that war was a pretty bloody stupid idea. At least the wholesale slaughter side of it. So they fashioned themselves a tradition of Championship.\n\nHere's how an Ezomyte battle would go. Both sides get dressed up in their fiercest and finest war garb. They'd all prance about, banging drums and flinging curses at each other. Then, when everyone was het up enough, a few chosen lads or lasses would step forward and fight it out...to the death usually.\n\nThe point of it all? My champions win, and I get to have that fish-filled estuary on my border. Your champions win, and I hand over a few hundred cows. Deal done, and most of us get to go home.\n\nThe Eternals pinched the idea from the Ezomytes, and the Oriathans pinched the idea from the Eternals.";
+	const char		*long_string = "\n\nDo you know where the duelist tradition came from, exile?\n\nThe Ezomytes.\n\nEarly on, those western potato munchers worked out that war was a pretty bloody stupid idea. At least the wholesale slaughter side of it. So they fashioned themselves a tradition of Championship.\n\nHere's how an Ezomyte battle would go. Both sides get dressed up in their fiercest and finest war garb. They'd all prance about, banging drums and flinging curses at each other. Then, when everyone was het up enough, a few chosen lads or lasses would step forward and fight it out...to the death usually.\n\nThe point of it all? My champions win, and I get to have that fish-filled estuary on my border. Your champions win, and I hand over a few hundred cows. Deal done, and most of us get to go home.\n\nThe Eternals pinched the idea from the Ezomytes, and the Oriathans pinched the idea from the Eternals.";
 
-	char			receptacle[4096];
-	char			receptacle2[4096];
+	char			receptacle[4096]; // buffer for testing purposes
+	char			receptacle2[4096]; // buffer for testing purposes
+	bzero(receptacle, 4096);
+	bzero(receptacle2, 4096);
 
-	ft_puts("-------------------- TEST ft_cat --------------------");
-	ft_bzero(receptacle, 4096);
-	ft_puts("medium_string : ");
-	ft_puts(medium_string);
-	ft_puts("medium_string put in empty buffer : ");
+	putstr_ghetto("\n\n-------------------- TEST ft_isalpha --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | 'a' | ");
+	putnbr_ghetto(ft_isalpha('a'));
+	putstr_ghetto("\nTest 2 | 'z' | ");
+	putnbr_ghetto(ft_isalpha('z'));
+	putstr_ghetto("\nTest 3 | 'A' | ");
+	putnbr_ghetto(ft_isalpha('A'));
+	putstr_ghetto("\nTest 4 | 'Z' | ");
+	putnbr_ghetto(ft_isalpha('Z'));
+	putstr_ghetto("\nTest 5 | '[' | ");
+	putnbr_ghetto(ft_isalpha('['));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_isdigit --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0' | ");
+	putnbr_ghetto(ft_isdigit('0'));
+	putstr_ghetto("\nTest 2 | '9' | ");
+	putnbr_ghetto(ft_isdigit('9'));
+	putstr_ghetto("\nTest 3 | 'A' | ");
+	putnbr_ghetto(ft_isdigit('A'));
+	putstr_ghetto("\nTest 3 | ':' | ");
+	putnbr_ghetto(ft_isdigit(':'));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_isalnum --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0' | ");
+	putnbr_ghetto(ft_isalnum('0'));
+	putstr_ghetto("\nTest 2 | '9' | ");
+	putnbr_ghetto(ft_isalnum('9'));
+	putstr_ghetto("\nTest 3 | 'A' | ");
+	putnbr_ghetto(ft_isalnum('A'));
+	putstr_ghetto("\nTest 4 | ':' | ");
+	putnbr_ghetto(ft_isalnum(':'));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_isascii --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0'   | ");
+	putnbr_ghetto(ft_isascii('0'));
+	putstr_ghetto("\nTest 2 | 'del' | ");
+	putnbr_ghetto(ft_isascii(0x7f));
+	putstr_ghetto("\nTest 2 | 2049  | ");
+	putnbr_ghetto(ft_isascii(2049));
+	
+	putstr_ghetto("\n\n-------------------- TEST ft_isprint --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0'   | ");
+	putnbr_ghetto(ft_isprint('0'));
+	putstr_ghetto("\nTest 2 | 'del' | ");
+	putnbr_ghetto(ft_isprint(0x7f));
+	putstr_ghetto("\nTest 2 | 2049  | ");
+	putnbr_ghetto(ft_isprint(2049));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_toupper --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0'   | ");
+	putnbr_ghetto(ft_toupper('0'));
+	putstr_ghetto("\nTest 2 | 2049  | ");
+	putnbr_ghetto(ft_toupper(2049));
+	putstr_ghetto("\nTest 3 | 'F'   | ");
+	putnbr_ghetto(ft_toupper('F'));
+	putstr_ghetto("\nTest 4 | 'g'   | ");
+	putnbr_ghetto(ft_toupper('g'));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_tolower --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | '0'   | ");
+	putnbr_ghetto(ft_tolower('0'));
+	putstr_ghetto("\nTest 2 | 2049  | ");
+	putnbr_ghetto(ft_tolower(2049));
+	putstr_ghetto("\nTest 3 | 'F'   | ");
+	putnbr_ghetto(ft_tolower('F'));
+	putstr_ghetto("\nTest 4 | 'g'   | ");
+	putnbr_ghetto(ft_tolower('g'));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_strcat --------------------\n");
+	putstr_ghetto("Short_string = \"");
+	putstr_ghetto(short_string);
+	putstr_ghetto("\"\nMedium_string = \"");
+	putstr_ghetto(medium_string);
+	putstr_ghetto("\"\nEmpty string into empty buffer  | ");
+	ft_strcat(receptacle, empty_string);
+	putstr_ghetto(receptacle);
+	putstr_ghetto("\nMedium string into empty buffer | ");
 	ft_strcat(receptacle, medium_string);
-	ft_puts(receptacle);
-	ft_puts("short string \"42\" put in buffer already filled with medium_string ");
+	putstr_ghetto(receptacle);
+	putstr_ghetto("\nShort string into same buffer   | ");
 	ft_strcat(receptacle, short_string);
-	ft_puts(receptacle);
+	putstr_ghetto(receptacle);
+	putstr_ghetto("\nEmpty string in same buffer     | ");
+	ft_strcat(receptacle, empty_string);
+	putstr_ghetto(receptacle);
 
-	ft_puts("-------------------- TEST ft_isalpha --------------------");
-	ft_puts("Test 1 : 'a'\n Test 2 = 'z' Test3 = 'A' Test 4 = 'Z' Test 5 = '[' ");
-	if (ft_isalpha(lowcase) != 0)
-		ft_puts("Test 1 = alpha");
-	else
-		ft_puts("Test 1 = not alpha");
+	putstr_ghetto("\n\n-------------------- TEST ft_bzero --------------------\n");
+	putstr_ghetto("[buffer reference]\n");
+	dumpmem(receptacle, 20);
+	ft_bzero(receptacle, 0);
+	putstr_ghetto("\n\n[first 20 bytes of buffer after bzero of size 0 at start of buffer...]\n");
+	dumpmem(receptacle, 20);
+	ft_bzero(receptacle, 5);
+	putstr_ghetto("\n\n[first 20 bytes of buffer after bzero of size 5 at start of buffer...]\n");
+	dumpmem(receptacle, 20);
 
-	int fd;
-	fd = open("auteur", O_RDONLY);
-	ft_cat(fd);
+	putstr_ghetto("\n\n-------------------- TEST ft_puts --------------------\n");
+	putstr_ghetto(">---[empty string]----<\n");
+	ft_puts(empty_string);
+	putstr_ghetto("\n>---[short string]----<\n");
+	ft_puts(short_string);
+	putstr_ghetto("\n>---[medium string]---<\n");
+	ft_puts(medium_string);
+	putstr_ghetto("\n>---[long string]-----<\n");
+	ft_puts(long_string);
+	putstr_ghetto("\n>---[NULL string]-----<\n");
+	ft_puts(NULL);
+
+	putstr_ghetto("\n-------------------- TEST ft_strlen --------------------\n");
+	putstr_ghetto("Return values\nTest 1 | empty string  | ");
+	putnbr_ghetto(ft_strlen(empty_string));
+	putstr_ghetto("\nTest 2 | short string  | ");
+	putnbr_ghetto(ft_strlen(short_string));
+	putstr_ghetto("\nTest 2 | medium string | ");
+	putnbr_ghetto(ft_strlen(medium_string));
+	putstr_ghetto("\nTest 2 | long string   | ");
+	putnbr_ghetto(ft_strlen(long_string));
+
+	putstr_ghetto("\n\n-------------------- TEST ft_memset --------------------\n");
+	putstr_ghetto("Remember our good friend [buffer reference] ? Well guess what he's back\n");
+	dumpmem(receptacle, 20);
+	ft_memset(receptacle, 42, 0);
+	putstr_ghetto("\n\n[first 20 bytes of buffer after memset value 42 of size 0 at start of buffer...]\n");
+	dumpmem(receptacle, 20);
+	ft_memset(receptacle, 42, 10);
+	putstr_ghetto("\n\n[first 20 bytes of buffer after memset value 42 of size 10 at start of buffer...]\n");
+	dumpmem(receptacle, 20);
+
+	putstr_ghetto("\n\n-------------------- TEST ft_memcpy --------------------\n");
+	putstr_ghetto("To dispence some righteous justice, [buffer reference] brought his sidekick [buffer2] !\n");
+	dumpmem(receptacle2, 20);
+	putstr_ghetto("\n\n[first 20 bytes of buffer2 after memcpy of size 0\n");
+	ft_memcpy(receptacle2, receptacle, 0);
+	dumpmem(receptacle2, 20);
+	putstr_ghetto("\n\n[first 20 bytes of buffer2 after memcpy of size 15\n");
+	ft_memcpy(receptacle2, receptacle, 15);
+	dumpmem(receptacle2, 20);
+
+	putstr_ghetto("\n\n-------------------- TEST ft_strdup --------------------\n");
+
+	char			*dup_empty;
+	char			*dup_short;
+	char			*dup_medium;
+	char			*dup_long;
+
+	putstr_ghetto("duplicated empty_string  | ");
+	dup_empty = ft_strdup(empty_string);
+//	dup_empty = strdup(empty_string);
+	put_size_t_ghetto((size_t)dup_empty);
+	putstr_ghetto("\n");
+	putstr_ghetto(dup_empty);
+
+	putstr_ghetto("\nduplicated short_string  | ");
+	dup_short = ft_strdup(short_string);
+//	dup_short = strdup(short_string);
+	put_size_t_ghetto((size_t)dup_short);
+	putstr_ghetto("\n");
+	putstr_ghetto(dup_short);
+
+	putstr_ghetto("\nduplicated medium_string | ");
+	dup_medium = ft_strdup(medium_string);
+//	dup_medium = strdup(medium_string);
+	put_size_t_ghetto((size_t)dup_medium);
+	putstr_ghetto("\n");
+	putstr_ghetto(dup_medium);
+
+	putstr_ghetto("\nduplicated long_string   | ");
+	dup_long = ft_strdup(long_string);
+//	dup_long = strdup(long_string);
+	put_size_t_ghetto((size_t)dup_long);
+	putstr_ghetto("\n");
+	putstr_ghetto(dup_long);
+
+//	free(dup_empty);
+//	free(dup_short);
+//	free(dup_medium);
+//	free(dup_long);
 
 	return 0;
 }
